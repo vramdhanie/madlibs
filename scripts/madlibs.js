43f9b53a0b1a24b691b9fecdd7531dd45c7e4e82
Vincent Ramdhanie <vramdhanie@gmail.com>
2015-01-04 11:06:07 -0400

    Style the button and add view switching

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -3,4 +3,15 @@ angular.module('madApp', [])
     .controller('MadController', function(VERSION, $scope){
         $scope.version = VERSION;
         $scope.male = false;
+        $scope.display = false;
+        $scope.showErrors =false;
+    
+        $scope.generate = function(){
+            //console.log($scope.madform.$valid);
+            $scope.display = $scope.showErrors = $scope.madform.$valid;
+        };
+        $scope.reset = function(){
+            $scope.female_name = "";
+            $scope.display = false;
+        };
     });
